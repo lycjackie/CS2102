fc117384205f2de78e985bbbd612a430817776ed
Jackie <leeyc@comp.nus.edu.sg>
2018-09-15 22:41:24 +0800

    added db connecter

--- flask/app.py
+++ flask/app.py
@@ -1,5 +1,42 @@
+#!/usr/bin/python
 from flask import Flask, render_template
+import psycopg2
+from dbconfig import config
+
+
 app = Flask(__name__, template_folder='templates')
+
+def connect():
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        # read connection parameters
+        params = config()
+
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**params)
+
+        # create a cursor
+        cur = conn.cursor()
+
+ # execute a statement
+        print('PostgreSQL database version:')
+        cur.execute('SELECT version()')
+
+        # display the PostgreSQL database server version
+        db_version = cur.fetchone()
+        print(db_version)
+
+     # close the communication with the PostgreSQL
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        return conn
+
+dbconn = connect()
+
 
 @app.route('/')
 def main():
