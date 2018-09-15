d9f8abe94c71077b221e50e33770dd13e16039a6
Jackie <leeyc@comp.nus.edu.sg>
2018-09-16 02:58:59 +0800

    Simple Queries done for rides. Modified the ER.sql for easier reading.
    - psycopg2 have NamedTupleCursor for easier access via '.' method
    - SQL added more foreigner key constrain for cascade
    - Sample under __main__

--- ER.sql
+++ ER.sql
@@ -1,81 +1,86 @@
+create database cs2102_project
+with owner postgres;
+
 create table if not exists "Car"
 (
-  "Model" varchar(64) not null
-    constraint car_pk
-    primary key,
+  "Model" varchar(64) not null,
   "Maker" varchar(64) not null,
-  "Seats" numeric(64) not null
+  "Seats" numeric(64) not null,
+  constraint car_pk
+  primary key ("Model")
 );
 
 create table if not exists "Users"
 (
   "Name"    varchar(64) not null,
-  "NRIC"    varchar(64) not null
-    constraint users_pk
-    primary key,
-  "Contact" numeric     not null
+  "NRIC"    varchar(64) not null,
+  "Contact" numeric     not null,
+  constraint users_pk
+  primary key ("NRIC")
 );
 
 create table if not exists "UserCar"
 (
-  "VehicleNo" varchar(12) not null
-    constraint usercar_pk
-    primary key,
+  "VehicleNo" varchar(12) not null,
   "Color"     varchar(64) not null,
-  "Model"     varchar(64) not null
-    constraint "UserCar_fk0"
-    references "Car",
-  "NRIC"      varchar(64) not null
-    constraint "UserCar_fk1"
-    references "Users"
-);
-
-create table if not exists "Rides"
-(
-  "RideId"       serial      not null
-    constraint rides_pk
-    primary key,
-  "Driver"       varchar(64) not null
-    constraint "Rides_fk0"
-    references "Users",
-  "RideDateTime" date        not null,
-  "RideEndTime"  date        not null,
-  "Origin"       varchar(64) not null,
-  "Destination"  varchar(64) not null,
-  "Status"       varchar(64) not null
-);
-
-create table if not exists "RideBids"
-(
-  "UserBid"    varchar(64) not null
-    constraint "RideBids_fk0"
-    references "Users",
-  "RideId"     integer     not null
-    constraint "RideBids_fk1"
-    references "Rides",
-  "BidPrice"   numeric     not null,
-  "Passengers" numeric     not null
+  "Model"     varchar(64) not null,
+  "NRIC"      varchar(64) not null,
+  constraint usercar_pk
+  primary key ("VehicleNo"),
+  constraint "UserCar_fk0"
+  foreign key ("Model") references "Car",
+  constraint "UserCar_fk1"
+  foreign key ("NRIC") references "Users"
 );
 
 create table if not exists "Roles"
 (
-  "RoleId"   serial      not null
-    constraint "Roles_pkey"
-    primary key,
-  "RoleName" varchar(64) not null
-    constraint "Roles_RoleName_key"
-    unique
+  "RoleId"   serial      not null,
+  "RoleName" varchar(64) not null,
+  constraint "Roles_pkey"
+  primary key ("RoleId"),
+  constraint "Roles_RoleName_key"
+  unique ("RoleName")
 );
 
 create table if not exists "UserRole"
 (
-  "User_NRIC" varchar(64) not null
-    constraint "UserRole_fk0"
-    references "Users"
-    on delete cascade,
-  "RoleId"    integer     not null
-    constraint "UserRole_fk1"
-    references "Roles"
-    on delete cascade
+  "User_NRIC" varchar(64) not null,
+  "RoleId"    integer     not null,
+  constraint "UserRole_fk0"
+  foreign key ("User_NRIC") references "Users"
+  on delete cascade,
+  constraint "UserRole_fk1"
+  foreign key ("RoleId") references "Roles"
+  on delete cascade
 );
 
+create table if not exists "Rides"
+(
+  "RideId"       serial      not null,
+  "Driver"       varchar(64) not null,
+  "RideDateTime" date        not null,
+  "RideEndTime"  time        not null,
+  "Origin"       varchar(64) not null,
+  "Destination"  varchar(64) not null,
+  "Status"       varchar(64) not null,
+  constraint rides_pk
+  primary key ("RideId"),
+  constraint "Rides_fk0"
+  foreign key ("Driver") references "Users"
+  on delete cascade
+);
+
+create table if not exists "RideBids"
+(
+  "UserBid"    varchar(64) not null,
+  "RideId"     integer     not null,
+  "BidPrice"   numeric     not null,
+  "Passengers" numeric     not null,
+  constraint "RideBids_fk0"
+  foreign key ("UserBid") references "Users"
+  on delete cascade,
+  constraint "RideBids_fk1"
+  foreign key ("RideId") references "Rides"
+  on delete cascade
+);
---
+++ flask/data/rides.py
@@ -0,0 +1,81 @@
+from dbconfig import connect
+import datetime as dt
+import psycopg2, psycopg2.extras
+
+'''
+create table if not exists "Rides"
+(
+  "RideId"       serial      not null
+    constraint rides_pk
+    primary key,
+  "Driver"       varchar(64) not null
+    constraint "Rides_fk0"
+    references "Users",
+  "RideDateTime" date        not null,
+  "RideEndTime"  date        not null,
+  "Origin"       varchar(64) not null,
+  "Destination"  varchar(64) not null,
+  "Status"       varchar(64) not null
+);
+cur.execute(
+     INSERT INTO some_table (an_int, a_date, another_date, a_string)
+      VALUES (%(int)s, %(date)s, %(date)s, %(str)s);,
+     {'int': 10, 'str': "O'Reilly", 'date': datetime.date(2005, 11, 18)})
+'''
+
+def addRide(ride_details):
+    sql = """
+        INSERT INTO \"Rides\" (\"Driver\", \"RideDateTime\", \"RideEndTime\", \"Origin\", \"Destination\",\"Status\")
+        VALUES (%s,%s,%s,%s,%s,%s) RETURNING \"RideId\";
+          """
+    db = connect()
+    res = None;
+    try:
+        cur = db.cursor()
+        cur.execute(sql,(ride_details['Driver'],ride_details['RideDateTime'],
+                         ride_details['RideEndTime'],ride_details['Origin'],
+                         ride_details['Destination'],ride_details['Status']))
+
+        res = cur.fetchone()[0] # successful insert will return the RideId
+
+        db.commit()
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print error
+    finally:
+        if db is not None:
+            db.close()
+    return res # return the RideId.
+
+
+def retrieveAllRide():
+
+    sql = """ SELECT * FROM \"Rides\" """
+    db = connect()
+    res = None
+    try:
+        cur = db.cursor(cursor_factory=psycopg2.extras.NamedTupleCursor) # nameTuple for easier access i.e using .Columns
+        cur.execute(sql)
+        res = cur.fetchall()
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print error
+    finally:
+        if db is not None:
+            db.close()
+    return res
+
+if __name__ == '__main__':
+    test_user = {
+        'Driver':'123456',
+        'RideDateTime': dt.date(2018,9,19),
+        'RideEndTime': dt.time(14,00),
+        'Origin': 'NUS',
+        'Destination': 'Verde Grove',
+        'Status': 'GONELOL'
+    }
+    #addUser(test_user)
+    #print addRide(test_user)
+    res = retrieveAllRide()
+    for ride in res:
+        print ride.Status # use NamedTuple
