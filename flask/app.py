ae2d34f25608e14ba426528717213dc862c7debf
coffeebaby <y.ch95@hotmail.com>
2018-09-16 16:58:11 +0800

    modified app.py to create rides with random data

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,25 @@
 #!/usr/bin/python
 from flask import Flask, render_template
 import psycopg2
+import datetime as dt
 import importlib
+import random
 
 app = Flask(__name__, template_folder='templates')
 ride = importlib.import_module("data.rides")
+
+origin = ["Toa Payoh", "NUS", "Woodlands", "Somewhere", "Orchard", "Sentosa"]
+destination = ["Punggol", "Buona Vista", "Paya Lebar", "Over There", "Ghim Moh", "Yew Tee"]
+currentDT = dt.datetime.now()
+
+test_driver = {
+	    'Driver':'123456',
+        'RideDateTime': dt.date(currentDT.year,currentDT.month,currentDT.day),
+        'RideEndTime': dt.time(currentDT.hour,currentDT.minute),
+        'Origin': str(origin[random.randint(0,5)]),
+        'Destination': str(destination[random.randint(0,5)]),
+        'Status': 'NOT_YET_GONE'
+    }
 
 @app.route('/')
 def main():
@@ -16,6 +31,7 @@ def main():
         'role': 3
     }
     '''
+    ride.addRide(test_driver);	
     list = ride.retrieveAllRide();
     return render_template('index.html', users = list);
 
