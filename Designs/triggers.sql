9c60b6f2023ffec6bc58bd81ed275d07620f661f
Jackie Lee <leeyc@comp.nus.edu.sg>
2018-10-12 12:41:03 +0800

    new trigger
    Trigger to check the capacity of the ride before insert

--- Designs/triggers.sql
+++ Designs/triggers.sql
@@ -20,3 +20,30 @@ $BODY$ LANGUAGE plpgsql;
   ON ride_bid
   FOR EACH ROW
   EXECUTE PROCEDURE on_approval_update_pax();
+
+
+CREATE OR REPLACE FUNCTION capacity_checker()
+  RETURNS trigger AS
+$func$
+BEGIN
+    IF ( SELECT (r.current_pax + NEW.no_pax <= m.capacity)
+    FROM ride r
+    inner join car c on r.reg_no = c.reg_no
+    INNER JOIN model m on c.make = m.make and c.model =m.model
+    AND r.reg_no = NEW.reg_no
+    AND r.start_time = NEW.start_time)
+      THEN
+      RETURN NEW;
+  ELSE
+  RETURN NULL;
+     END IF;
+END
+$func$  LANGUAGE plpgsql;
+
+
+
+	CREATE TRIGGER cap_check
+  BEFORE INSERT
+  ON ride_bid
+  FOR EACH ROW
+  EXECUTE PROCEDURE capacity_checker();
